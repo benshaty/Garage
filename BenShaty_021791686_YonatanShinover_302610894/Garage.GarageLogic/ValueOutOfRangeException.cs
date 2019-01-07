cb6cdb7716ec616bcdc13c902a906ec80bcb9509
Ben shaty <benshaty@gmail.com>
2019-01-07 20:38:06 +0200

    added ValueOutOfRangeException

---
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/ValueOutOfRangeException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.GarageLogic
+{
+    class ValueOutOfRangeException : Exception
+    {
+        public float MaxValue { get; set; }
+        public float MinValue { get; set; }
+    }
+}
