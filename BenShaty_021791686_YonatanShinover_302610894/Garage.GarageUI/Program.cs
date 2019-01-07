671fadeee1c9e947fe45470ad9945e178ff84f45
Ben shaty <benshaty@gmail.com>
2019-01-07 19:25:49 +0200

    fskdn

--- BenShaty_021791686_YonatanShinover_302610894/Garage.GarageUI/Program.cs
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageUI/Program.cs
@@ -9,11 +9,12 @@ namespace Garage.GarageUI
     class Program
     {
         public int MyProperty { get; set; }
-        public int cmkmkmd { get; set; }
+        public static int cmkmkmd { get; set; }
         public int hgfh { get; set; }
         static void Main(string[] args)
         {
-            
+            int z = 5;
+            cmkmkmd = z;
         }
     }
 }
