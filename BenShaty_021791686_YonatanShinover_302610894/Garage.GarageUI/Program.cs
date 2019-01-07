856199ce853ae528e90de183c56643b42b7b0d57
Ben shaty <benshaty@gmail.com>
2019-01-07 19:32:55 +0200

    added a logic project as class liberery

--- BenShaty_021791686_YonatanShinover_302610894/Garage.GarageUI/Program.cs
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageUI/Program.cs
@@ -3,15 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Garage.GarageLogic;
 
 namespace Garage.GarageUI
 {
     class Program
     {
-        
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
