4ba0f06cc9001e4db9e50d34b9984012de1668bc
Ben shaty <benshaty@gmail.com>
2019-01-07 19:22:16 +0200

    dsfdsfs dsfds

--- BenShaty_021791686_YonatanShinover_302610894/Garage.GarageUI/Program.cs
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageUI/Program.cs
@@ -10,6 +10,7 @@ namespace Garage.GarageUI
     {
         public int MyProperty { get; set; }
         public int cmkmkmd { get; set; }
+        public int hgfh { get; set; }
         static void Main(string[] args)
         {
             
