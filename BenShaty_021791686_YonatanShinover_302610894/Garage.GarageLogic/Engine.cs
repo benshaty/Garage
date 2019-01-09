e45d4601972b72caf281f3b450c7b4b9892f1702
Ben shaty <benshaty@gmail.com>
2019-01-09 18:14:56 +0200

    Finished Engine Class

--- BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/Engine.cs
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/Engine.cs
@@ -15,17 +15,17 @@ namespace Garage.GarageLogic
         /// <param name="ElectricAmount">Electric Amount to Add (float)</param>
         public virtual void AddEnergy(string LicensePlate, float ElectricAmount)
         {
-
+            throw new ArgumentException("This is an Gasoline Vehicle");
         }
         /// <summary>
         /// Add energy to an Gasoline Vehicle
         /// </summary>
         /// <param name="LicensePlate">License Plate (string)</param>
-        /// <param name="GasolineAmount"></param>
-        /// <param name="FuelType"></param>
+        /// <param name="GasolineAmount">amount of gasoline to add</param>
+        /// <param name="FuelType">fuel type Enum</param>
         public virtual void AddEnergy(string LicensePlate, float GasolineAmount, E_FuelType FuelType)
         {
-
+            throw new ArgumentException("This is an Electric Vehicle");
         }
     }
 }
