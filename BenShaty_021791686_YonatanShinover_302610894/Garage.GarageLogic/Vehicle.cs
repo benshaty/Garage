7a18328e0000e0fc9da93a485d970ff60b11278a
yoyoshino123 <yshinover10@gmail.com>
2019-01-09 23:00:55 +0200

    coments to vehicle class

--- BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/Vehicle.cs
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/Vehicle.cs
@@ -18,26 +18,39 @@ namespace Garage.GarageLogic
         public E_VehicleStateInGarage VehicleStateInGarage { get; set; }
         public Wheel[] VehicleWheels = new Wheel[4];
         public Engine VehicleEngine { get; set; }
+        /// <summary>
+        /// Override ToString method.
+        /// return string of all vehicle details
+        /// </summary>
+        /// <returns>string of all vehicle details</returns>
         public override string ToString()
         {
-            
+            // set basic properties text to result
             string res =  "Vehicle: "+ VehicleLicencePlate + ", " + VehicleModel + ", " + OwnerName + ", " + OwnerPhone + ", " + VehicleStateInGarage + " ";
+            // foreach loop to get all wheels details
             foreach (var wheel in VehicleWheels)
             {
+                // add details to result string
                 res += wheel.ManufacturerName + " " + wheel.WheelAirPresure + " ";
             }
+            // add engine type to result string
             res += VehicleEngine.EngineType + " " + VehicleEngine.CurrentAmount + " ";
+            // check if engine type is fuel and add data to result string
             switch (VehicleEngine.EngineType)
             {
                 case E_EngineType.Fuel:
+                    // cast the engine to fuel type
                     var tempEngine = (Gasoline)VehicleEngine;
+                    // add fuel engine fuel type
                     res += tempEngine.FuelType + " ";
                     break;
                 case E_EngineType.Electric:
                     break;
                 default:
+                    // no engine type is defined
                     throw new ArgumentException("Not defined vehicle engine type");
             }
+            // return result as string
             return res;
         }
     }
