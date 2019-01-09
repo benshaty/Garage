65c30cb07d40e5692f9623d6ce51b41c47402a85
Ben shaty <benshaty@gmail.com>
2019-01-09 23:23:07 +0200

    fixed parsers dll

--- BenShaty_021791686_YonatanShinover_302610894/Utils/Properties/AssemblyInfo.cs
+++ BenShaty_021791686_YonatanShinover_302610894/Utils/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e93c2fb1-84fe-413c-936a-8ce3aca7cff4")]
+[assembly: Guid("f711ddd0-c03c-44cc-9315-9ba4040436bf")]
 
 // Version information for an assembly consists of the following four values:
 //
