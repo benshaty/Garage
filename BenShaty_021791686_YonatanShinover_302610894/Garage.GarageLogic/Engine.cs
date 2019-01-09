aaf983e6352a62512460dce4fc25c142097a083f
Ben shaty <benshaty@gmail.com>
2019-01-09 18:09:48 +0200

    Added value out of range exeption

---
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/Engine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.GarageLogic
+{
+    public class Engine
+    {
+        /// <summary>
+        /// Add energy to an Electric Vehicle
+        /// </summary>
+        /// <param name="LicensePlate">License Plate (string)</param>
+        /// <param name="ElectricAmount">Electric Amount to Add (float)</param>
+        public virtual void AddEnergy(string LicensePlate, float ElectricAmount)
+        {
+
+        }
+        /// <summary>
+        /// Add energy to an Gasoline Vehicle
+        /// </summary>
+        /// <param name="LicensePlate">License Plate (string)</param>
+        /// <param name="GasolineAmount"></param>
+        /// <param name="FuelType"></param>
+        public virtual void AddEnergy(string LicensePlate, float GasolineAmount, E_FuelType FuelType)
+        {
+
+        }
+    }
+}
--- BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/ValueOutOfRangeException.cs
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/ValueOutOfRangeException.cs
@@ -8,7 +8,34 @@ namespace Garage.GarageLogic
 {
     class ValueOutOfRangeException : Exception
     {
-        public float MaxValue { get; set; }
-        public float MinValue { get; set; }
+        private float MaxValue;
+        private float MinValue;
+        public string CustomMassege { get; set; }
+        public ValueOutOfRangeException()
+        {
+
+        }
+        public ValueOutOfRangeException(string massage) : base(massage)
+        {
+
+        }
+        public ValueOutOfRangeException(string massage, float maxValue, float minValue, float value) : base(massage)
+        {
+            MaxValue = maxValue;
+            MinValue = minValue;
+            CustomMassege = checkErr(MaxValue,MinValue,value);
+        }
+        private string checkErr(float max, float min , float value)
+        {
+            string returnData = "";
+            if (value > max)
+            {
+                returnData = "Value is Bigger then Max";
+            } else if (min > value)
+            {
+                returnData = "Value is Smaller the Min";
+            }
+            return returnData;
+        }
     }
 }
