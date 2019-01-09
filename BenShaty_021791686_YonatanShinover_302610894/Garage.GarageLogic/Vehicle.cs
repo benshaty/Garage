d437c5ad4cdaf02aaa39fd2b6f1fbd8138b3a859
yoyoshino123 <yshinover10@gmail.com>
2019-01-09 22:00:56 +0200

    changed E_CarStateInGarage E_VehicleStateInGarage

---
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.GarageLogic
+{
+    class Vehicle
+    {
+        public string VehicleModel { get; set; }
+        public string VehicleLicencePlate { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerPhone { get; set; }
+        public Engine VehicleEngine { get; set; }
+        public E_VehicleStateInGarage VehicleStateInGarage { get; set; }
+
+    }
+}
