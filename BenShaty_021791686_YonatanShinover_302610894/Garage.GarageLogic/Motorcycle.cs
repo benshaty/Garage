2771d89b1fb728003be978ebfef9a75991e3f95c
yoyoshino123 <yshinover10@gmail.com>
2019-01-10 11:42:47 +0200

    Created the Motorcycle Class and Properties + Comments

---
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/Motorcycle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.GarageLogic
+{
+    /// <summary>
+    /// This is the Motorcycle Class
+    /// </summary>
+    class Motorcycle : Vehicle
+    {
+        /// <summary>
+        /// The License type of the Motorcycle
+        /// </summary>
+        public E_LicenseType LicenseType { get; set; }
+        /// <summary>
+        /// This is the Engine Volume of the Motorcycle
+        /// </summary>
+        public int EngineVolume { get; set; }
+    }
+}
