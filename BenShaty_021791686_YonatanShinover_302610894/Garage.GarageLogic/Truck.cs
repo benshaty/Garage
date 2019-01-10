32f08a9f6b39ff12adcf46ccf3e1088eb20be27e
yoyoshino123 <yshinover10@gmail.com>
2019-01-10 11:53:40 +0200

    Created the Truck Class and Properties+Comments

---
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/Truck.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.GarageLogic
+{
+    /// <summary>
+    /// This is the Truck Class inheriting from Vehicle
+    /// </summary>
+    class Truck : Vehicle
+    {
+        /// <summary>
+        /// This is the True/False Hazardous materials status of the Truck 
+        /// </summary>
+        public bool HazardousMaterials { get; set; }
+        /// <summary>
+        /// This is the Max weight amount a Truck can lift
+        /// </summary>
+        public float MaxWeight { get; set; }
+
+    }
+}
