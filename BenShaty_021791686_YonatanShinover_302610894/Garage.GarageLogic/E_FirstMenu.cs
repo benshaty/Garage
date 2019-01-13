e2106faa90eb47286a2bacaee835ad6d8a6d23ba
Ben shaty <benshaty@gmail.com>
2019-01-13 22:41:19 +0200

    added first menu display

---
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageLogic/E_FirstMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.GarageLogic
+{
+    public enum E_FirstMenu
+    {
+        Exit_system,
+        Add_a_vehicle,
+        Show_vehicles_by_license_plate,
+        Show_vehicles_by_license_plate_with_filter,
+        Change_car_state,
+        Add_air_to_vehicle_wheels,
+        Add_energy_to_vehicle,
+        Show_vehicle_full_details
+    }
+}
--- BenShaty_021791686_YonatanShinover_302610894/Garage.GarageUI/GarageApp.cs
+++ BenShaty_021791686_YonatanShinover_302610894/Garage.GarageUI/GarageApp.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Garage.GarageLogic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,17 @@ namespace Garage.GarageUI
         public static void StartApp()
         {
             Console.WriteLine("Welcome to our garage");
+            showmenu();
+        }
+
+        private static void showmenu()
+        {
+            Console.WriteLine("=============================================");
+            foreach (var item in Enum.GetValues(typeof(E_FirstMenu)))
+            {
+                Console.WriteLine($"{(int) item} - {item.ToString().Replace("_"," ")}");
+            }
+            Console.Write("Enter menu item:");
         }
     }
 }
